48bd04698594916c0503675ba9d8b6b710dfd9ce
nlewis84 <nathan@differential.com>
2021-10-07 13:37:20 -0500

    cleaned up unneeded cases in switch statement

--- apollos-church-api/src/data/ContentItem.js
+++ apollos-church-api/src/data/ContentItem.js
@@ -14,18 +14,6 @@ const resolver = {
               paper: 'white',
             },
           };
-        // case '1acff1cb-805b-4561-b5f7-ee136b0b39b4': // Get Set
-        //   return {
-        //     colors: {
-        //       paper: 'salmon',
-        //     },
-        //   };
-        // case '569f5ff8-4cf9-4de0-a4d5-cd91ed78c7e2': // Go Serve
-        //   return {
-        //     colors: {
-        //       paper: 'black',
-        //     },
-        //   };
         default:
           return {
             colors: {
@@ -39,12 +27,6 @@ const resolver = {
     ...ContentItem.resolver.UniversalContentItem,
     theme: (args) => {
       switch (args.dataValues.contentItemCategoryId) {
-        // case '8edb84c2-063a-45bf-b92e-845745edb57a': // Be Ready
-        //   return {
-        //     colors: {
-        //       paper: 'white',
-        //     },
-        //   };
         case '1acff1cb-805b-4561-b5f7-ee136b0b39b4': // Get Set
           return {
             colors: {
