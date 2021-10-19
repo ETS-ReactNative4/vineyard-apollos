38a16765e1450ab99b4ec1789a6c5f21717e9e2f
Conrad VanLandingham <conrad@differential.com>
2021-10-19 12:45:28 -0500

    try moving introspection data cmd

--- apolloschurchapp/appcenter-post-clone.sh
+++ apolloschurchapp/appcenter-post-clone.sh
@@ -13,3 +13,5 @@ COCOAPODS_VER=$(sed -n -e 's/^COCOAPODS: \([0-9.]*\)/\1/p' ios/Podfile.lock)
 
 echo "Installing CocoaPods version $COCOAPODS_VER"
 sudo gem install cocoapods -v "$COCOAPODS_VER"
+
+node ./scripts/get-introspection-data.js
